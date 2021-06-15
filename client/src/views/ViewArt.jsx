b8bfa8c6a70f8a46e5271a30e4abfb89b31ffa78
sosoulfull <somaiah.95.sa@gmail.com>
2021-06-15 18:01:18 +0300

    6:01

--- client/src/views/ViewArt.jsx
+++ client/src/views/ViewArt.jsx
@@ -1,6 +1,4 @@
-import React, {useState} from 'react'
-import axios from 'axios'
-import {Link} from '@reach/router'
+import React from 'react';
 
 const ViewArt = props =>{
     console.log("if");
@@ -11,9 +9,33 @@ const ViewArt = props =>{
 
 
     return (
-        <div>
-            <a href={articles[id].url} target="_blank">Read on orignal Source</a>
+        <div style={{margin:"5px", padding:"5px", border:"solid black 1px", width:"1000px", position:"absolute"}}>
+
+            <div style={{padding:"5px", border:"solid black 1px", width: "200px"}}>
+                <p>asdfghjklkrtyuiouoytirdsazx</p>
+                <p>dhjcefkjvbfjhvbfekvcjbfj
+                    vcbhfkdj,vcfhbv
+                    jhfgjhmfy,jdkhgfxhmnz xcdvjkctxur
+                </p>
+                <p></p>
+                <p></p>
+            </div>
+
+
+            <div style={{marginLeft:"300px", padding:"5px", border:"solid black 1px" }}>
+                <p>{articles[id].title}</p>
+                <p>{articles[id].description}</p>
+                <p>{articles[id].content}</p>
+                <img src={`${articles[id].image}`} height="350px" width="700px"/>
+                <a href={articles[id].url} target="_blank">Read From the Orignal Source</a>
+                
+                <div style={{marginLeft:"200px", display:"inline-block"}} >
+                    published at: {articles[id].publishedAt}
+                </div>
+            </div>
+            
         </div>
+        
     )
 }
 
