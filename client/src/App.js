2589bc3bc59f6820abada0fd7c7b2f0f90c72c65
Abeer <Abeer.alamri1997@gmail.com>
2021-06-16 22:27:44 +0300

    msg

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-
 import './App.css';
 import Main from './views/Main';
 import ViewArt from './views/ViewArt'
 import Favorites from './views/Favorites'
+import ViewLocalArt from './views/ViewLocalArt'
 import {Router} from '@reach/router'
 import {useState} from 'react'
+
 
 function App() {
   const [articles, setArticles] = useState([]);
@@ -13,18 +14,18 @@ function App() {
     <div className="App">
           <div>
         <section className="curved">
-
-        <ul>
-            <li><a href="/">Search</a></li>
-            <li><a href="/favorites">Favorites</a></li>
-        </ul>
+          <ul>
+              <li><a href="/">Search</a></li>
+              <li><a href="/favorites">Favorites</a></li>
+          </ul>
         </section>
-   </div>
+    </div>
    
       <Router> 
             <Main path='/' articles={articles}  setArticles={setArticles} />
             <ViewArt path="/view/:id" articles={articles}  />
             <Favorites path='/favorites'/>
+            <ViewLocalArt path = '/articles/:id' articles={articles} setArticles={setArticles}/>
       </Router>
     </div>
   );
